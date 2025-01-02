dac98a7f17fb2aac5e14b024b25338c4735b787d
Diogenes Fernandes <diofeher@gmail.com>
2025-01-02 10:52:46 -0300

    feat: check gpg keys of providers
    Signed-off-by: Diogenes Fernandes <diofeher@gmail.com>

--- src/cmd/verify-gpg-key/main.go
+++ src/cmd/verify-gpg-key/main.go
@@ -48,6 +48,8 @@ func main() {
 	result.Steps = append(result.Steps, s)
 
 	// TODO: Add verification to ensure that the key has been used to sign providers in this github organization
+	s = VerifyKeySigningOrg(ghClient, *keyFile, *orgName)
+	result.Steps = append(result.Steps, s)
 
 	fmt.Println(result.RenderMarkdown())
 
@@ -87,26 +89,33 @@ func VerifyGithubUser(client github.Client, username string, orgName string) *ve
 
 var gpgNameEmailRegex = regexp.MustCompile(`.*\<(.*)\>`)
 
+func LoadGPGKey(location string) (*crypto.Key, error) {
+	// read the key from the filesystem
+	data, err := os.ReadFile(location)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read key file: %w", err)
+	}
+
+	key, err := gpg.ParseKey(string(data))
+	if err != nil {
+		return nil, fmt.Errorf("could not parse key: %w", err)
+	}
+	return key, nil
+}
+
 func VerifyKey(location string) *verification.Step {
 	verifyStep := &verification.Step{
 		Name: "Validate GPG key",
 	}
 
-	// read the key from the filesystem
-	data, err := os.ReadFile(location)
+	key, err := LoadGPGKey(location)
 	if err != nil {
-		verifyStep.AddError(fmt.Errorf("failed to read key file: %w", err))
+		verifyStep.AddError(err)
 		verifyStep.Status = verification.StatusFailure
-		return verifyStep
+		return nil
 	}
 
-	var key *crypto.Key
 	verifyStep.RunStep("Key is a valid PGP key", func() error {
-		k, err := gpg.ParseKey(string(data))
-		if err != nil {
-			return fmt.Errorf("could not parse key: %w", err)
-		}
-		key = k
 		return nil
 	})
 
@@ -166,6 +175,28 @@ func VerifyKey(location string) *verification.Step {
 				return fmt.Errorf("key identity %s has an invalid email: %w", idName, err)
 			}
 		}
+
+		return nil
+	})
+
+	emailStep.FailureToWarning()
+
+	return verifyStep
+}
+
+func VerifyKeySigningOrg(location string) *verification.Step {
+	verifyStep := &verification.Step{
+		Name: "Validate GPG key being used to sign providers in the organization",
+	}
+
+	key, err := LoadGPGKey(location)
+	if err != nil {
+		verifyStep.AddError(err)
+		verifyStep.Status = verification.StatusFailure
+		return nil
+	}
+
+	emailStep := verifyStep.RunStep("Key is being used to sign provider", func() error {
 
 		return nil
 	})
